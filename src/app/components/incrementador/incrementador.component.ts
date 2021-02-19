6b894465d12f7136376393ab676289676a99e2b5
Nicolas Gonzalez <nicolas.ignacio.gonzam@gmail.com>
2021-02-19 17:16:08 -0300

    Se creo un componente re utilizable incrementador

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,24 +1,45 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-incrementador',
   templateUrl: './incrementador.component.html',
   styleUrls: ['./incrementador.component.css']
 })
-export class IncrementadorComponent {
+export class IncrementadorComponent implements OnInit {
+
 
   @Input() progreso = 50;
+  @Input() btnClass = 'btn-primary';
+  @Output() valorSalida: EventEmitter<number> = new EventEmitter();
 
-
+  ngOnInit(): void {
+    this.btnClass = 'btn ' + this.btnClass;
+  }
 
   cambiarValor(valor: number): number{
     if (this.progreso >= 100 && valor >= 0){
+      this.valorSalida.emit(100);
       return this.progreso = 100;
     }
     if (this.progreso <= 0 && valor < 0){
+      this.valorSalida.emit(0);
       return this.progreso = 0;
     }
-    return this.progreso += valor;
+    const emitirValor = this.progreso += valor;
+    this.valorSalida.emit(emitirValor);
+    return emitirValor;
+  }
+  onChange(nuevoValor: any): void{
+
+    if (nuevoValor >= 100){
+      this.progreso = 100;
+    }else if (nuevoValor <= 0){
+      this.progreso = 0;
+    } else {
+      this.progreso = nuevoValor;
+    }
+    this.valorSalida.emit(this.progreso);
   }
 
 }
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
   ]
 })
 export class ProgressComponent {
+  progreso1 = 25;
+  progreso2 = 35;
+
+  get getProgreso1(): string{
+    return `${this.progreso1}%`;
+  }
+  get getProgreso2(): string{
+    return `${this.progreso2}%`;
+  }
+
+  cambioValorHijo(valor: number): void{
+    console.log('hola', valor);
+  }
 
 }
