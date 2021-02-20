f16810675c6620656214d428ae8e8d65d542e9cf
Nicolas Gonzalez <nicolas.ignacio.gonzam@gmail.com>
2021-02-20 15:35:50 -0300

    unsubscribe

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { Observable, interval } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import { Observable, interval, Subscription } from 'rxjs';
 import { retry, take, map, filter } from 'rxjs/operators';
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
   styleUrls: ['./rxjs.component.css']
 })
-export class RxjsComponent {
-
+export class RxjsComponent implements OnDestroy {
+  intervalo: Subscription;
   constructor() {
     // this.retornaObservable().pipe(
     //   retry()
@@ -16,13 +16,16 @@ export class RxjsComponent {
     //   err => console.warn('Error:', err),
     //   () =>  console.log('obs terminado'));
 
-    this.retornaIntervalo().subscribe(console.log);
+    this.intervalo = this.retornaIntervalo().subscribe(console.log);
    }
+  ngOnDestroy(): void {
+    this.intervalo.unsubscribe();
+  }
 
 retornaIntervalo(): Observable<number>{
-  return interval(500)
+  return interval(100)
   .pipe(
-    take(10),
+    // take(10),
     map(valor => valor + 1),
     filter( valor => (valor % 2 === 0)),
     );
