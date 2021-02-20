51e7a8a1530b6a40d76febaec446cc9ad0580669
Nicolas Gonzalez <nicolas.ignacio.gonzam@gmail.com>
2021-02-20 15:04:15 -0300

    Observables subscribe, error y complete

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
   styleUrls: ['./rxjs.component.css']
 })
-export class RxjsComponent implements OnInit {
+export class RxjsComponent {
 
-  constructor() { }
+  constructor() {
+    const $obs = new Observable( observer => {
+      let i = 0;
+      const intervalo = setInterval(() => {
+        i++;
+        observer.next(i);
+        if (i === 4){
+          clearInterval(intervalo);
+          observer.complete();
+        }
+        if (i === 2){
+          clearInterval(intervalo);
+          observer.error('i llego al valor de 2');
+        }
+      }, 1000);
+    });
+    $obs.subscribe(
+      valor => console.log('Subs', valor),
+      err => console.warn('Error:', err),
+      () =>  console.log('obs terminado'));
+   }
 
-  ngOnInit(): void {
-  }
 
 }
